9341d285d27e48130d904c8726ae57b5f9e6cf6d
Yachint <yachint@supplynote.in>
2023-12-27 14:17:00 +0530

    feature: change greeter text!

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React right now bitch!
+          Learn React right now bitch! (or batch?)
         </a>
       </header>
     </div>
